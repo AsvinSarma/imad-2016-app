2ed9145576cc95493fdd760cf5b73d4e60b45abc
AsvinSarma <amraspn@gmail.com>
2016-10-05 20:34:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,12 @@ app.get('/:articleName',function(req,res){
     var articleName =req.params.articleName;
      res.send(createTemplate(articles[articleName]));
 });
-
+app.get('/article-two', function (req, res) {
+     res.sendfile(path.join(_dirname,'ui', 'article-two.html'));
+    });
+    app.get('/article-three', function (req, res) {
+     res.sendfile(path.join(_dirname,'ui', 'article-three.html'));
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
