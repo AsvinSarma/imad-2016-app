7722ec7cf6fb04f2727c9b6d6f707eefc7b2dfd8
AsvinSarma <amraspn@gmail.com>
2016-10-18 08:53:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-<!doctype html>
-<html>
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div class="center">
-                <img src="C:\Users\ps\Desktop\asapp.jpg" class="img-medium"/>
-            </div>
-            <hr/>
-            <h3>Personal</h3>
-            <div>
-                Hi freinds welcome to my first webapp.
-            </div>
-            <br>
-            <hr/>
-            <h3>Deatils</h3>
-            <div>
-                Hi freinds I'm studying in plus two.
-            </div>
-            <hr/>
-            <div id="footer">
-            </div>
-        </div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>
+var express=require('express');
+var morgan=require('morgan');
+var path=require('path');
+
+var app=express();
+app.use(morgan('combined'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port=8080; //Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function(){
+    console.log('IMAD cource app listening on port ${port}!');
+});
