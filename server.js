311b87b09e0769f25fd34fc97ea2046a935ab73f
AsvinSarma <amraspn@gmail.com>
2016-10-18 08:40:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,102 +1,29 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-var articles = {
-    'article-one':{
-       title:'Article One',
-       heading:'Article One',
-       date:'Oct 2 2016',
-       content:`
-               <p>
-               This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.
-                    </p>
-              
-                     <p>This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.
-                     </p>
-                     <p>This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.This is the first article of my webapp.
-                     </p>`
-    },
-    'article-two' :{
-        title:'Article Two',
-        heading:'Article Two',
-        date:'Oct 5 2016',
-        content:`
-               <p>
-               This is the second article of my webapp.This is the second article of my webapp.
-               </p>`
-    },
-    'article-three':{ 
-        title:'Article Three',
-        heading:'Article Three',
-        date:'Oct 8 2016',
-        content:`
-               <p>
-               This is the third article of my webapp.This is the third article of my webapp.
-                </p>`
-    }
-};
-
-function createTemplate (data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    var htmlTemplate =`
-      <html>
-        <head>
-            <title>
-                  ${title}
-            </title>
-        <meta name="viewport" content="width=device-width initial scale=1">
-         <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-          <div class="container">
-                <div>
-                        <a href="/">Home</a>
-                </div>
-                    <hr/>
-                    <h3>
-                        ${heading}
-                    </h3>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                 ${content}
-                  </div>
-             </div>
-        </body>
-    </html>
-    `;
-    return htmlTemplate;
-}
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/:articleName',function(req,res){
-    // articleName == article-one
-    // articles[articleName] == {} content object for article one
-    var articleName =req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="center">
+                <img src="C:\Users\ps\Desktop\asapp.jpg" class="img-medium"/>
+            </div>
+            <hr/>
+            <h3>Personal</h3>
+            <div>
+                Hi freinds welcome to my first webapp.
+            </div>
+            <br>
+            <hr/>
+            <h3>Deatils</h3>
+            <div>
+                Hi freinds I'm studying in plus two.
+            </div>
+            <hr/>
+            <div id="footer">
+            </div>
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
