9a3c67cab818cbe729ab8acfcf9f4b6dcd15e724
AsvinSarma <amraspn@gmail.com>
2016-10-18 09:31:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,8 @@ var htmlTemplate = `
 
 
 app.get('/:articleName', function(req,res) {
+    // articleName=article-one
+    //articles[articleName]== {}content object for article one
   var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
