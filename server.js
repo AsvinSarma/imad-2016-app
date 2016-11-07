132b5899241fc2fce3a58cdbb9414c26f57ea8f3
AsvinSarma <amraspn@gmail.com>
2016-11-07 21:07:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express=require('express');
 var morgan=require('morgan');
 var path=require('path');
-var pool=require('pg').Pool;
+var Pool=require('pg').Pool;
 
-var config = {
+var config ={
     user:'asvinsarma',
-    database:'asvinsarma',
-    host:'http://db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DD_PASSWORD
-
+    database: 'asvinsarma',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
 };
+
 var app=express();
 app.use(morgan('combined'));
 
@@ -85,14 +85,16 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool= new Pool(config);
-app.get('/test-db',function(req,res) {
-    pool.query('SELECT * FROM article',function(err,result){
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return the response with the results
+    pool.query('SELECT * FROM article', function(err,result) {
         if(err){
-            res.status(500).send(err.toString());
-        }else{
-            res.send(JSON.stringify(result.rows));
-         }
+            res.send(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
     });
 });
 var counter =0;
@@ -100,24 +102,7 @@ app.get('/counter', function (req, res) {
     counter = counter+1;
     res.send(counter.toString());
 });
-app.get('/articles/:articleName', function(req,res) {
- //articleName == article-one
- //articles[articleName] == {} content object for article one
- 
-  pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName + "'", function(err,result) {
-      if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (result.rows.length === 0) {
-                res.status(404).send('Article not found');
-            } else {
-            
-                var articleData= result.rows[0];
-                res.send(createTemplate(articleData));
-            }
-        }
-  });
-});
+
 
 app.get('/ui/style.css', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
