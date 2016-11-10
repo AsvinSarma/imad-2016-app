3fc9ba88e69fff91e2a30001b3e7e031420105d8
AsvinSarma <amraspn@gmail.com>
2016-11-10 06:03:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express=require('express');
 var morgan=require('morgan');
 var path=require('path');
 var Pool=require('pg').pool;
-var crypto= require('crypto')
+var crypto= require('crypto');
 
 
 
