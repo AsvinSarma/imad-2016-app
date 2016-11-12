d4c505f48e8415bb8cc98b7851cacc0425057b74
AsvinSarma <amraspn@gmail.com>
2016-11-12 19:03:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,13 +132,11 @@ app.get('/test-db',function(req,res)  {
     });
 });
 
-
-var counter =0;
-app.get('/counter', function (req, res) {
-    counter = counter+1;
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
     res.send(counter.toString());
 });
-
 
 app.get('/articles/:articleName', function(req,res){
 
