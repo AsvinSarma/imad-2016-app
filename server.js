f6868bccae8be76ff77d4cf1159f8aed9aafed24
AsvinSarma <amraspn@gmail.com>
2016-11-21 12:51:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,6 +177,8 @@ app.get('/articles/:articleName', function(req,res) {
     }
   });  
 });
+
+
 app.get('/ui/style.css', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
