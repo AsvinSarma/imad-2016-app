f4cb0e3b10f029bf0b456ce0f496b41b7ce6e0e8
AsvinSarma <amraspn@gmail.com>
2016-11-23 18:43:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
+
 //Submit username/password to login
-var submit =document.getElementById('submit_btn');
+var submit = document.getElementById('submit_btn');
 submit.onclick =function(){
     //Create a request object
     var request =new XMLHttpRequest();
@@ -9,12 +10,11 @@ submit.onclick =function(){
         if(request.readyState ===XMLHttpRequest.DONE){
             //Take some action
             if (request.status ===200){
-                
-              alert('Logged in Succyessfully');
+              alert('Logged in Successfully');
             }else if(request.status===403){
                 alert('Username/password is wrong');
             }else if(request.status===500){
-                alert('Something went wrong');
+                alert('Something went wrong on the server');
             }
 }
 //Not done yet
@@ -24,6 +24,6 @@ var username=document.getElementById('username').value;
 var password=document.getElementById('password').value;
 console.log(username);
 console.log(password);
-request.open('POST','http://asvinsarma.imad.hasura-app.io/login',true);
-request.send(JSON.stringify({username:username, password:password}));
+request.open('POST', 'http://asvinsarma.imad.hasura-app.io/login', true);
+request.send(JSON.stringify({username: username, password: password}));
 };
