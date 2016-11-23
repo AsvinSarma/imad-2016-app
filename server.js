ed5e3c0fd6b35277348c1d03fd8de9fb52ef21c6
AsvinSarma <amraspn@gmail.com>
2016-11-23 06:25:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path=require('path');
 var Pool=require('pg').Pool;
 var crypto= require('crypto');
 var bodyParser =require('body-parser');
+var session =require('express-session');
 var config={
       user:'asvinsarma',
       database:'asvinsarma',
@@ -17,6 +18,10 @@ var config={
 var app=express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'someRandomSecretValue',
+    cookie:{maxAge: 1000 * 60 * 60 * 24 *30}
+}));
 
 
 var articles ={
@@ -135,6 +140,10 @@ app.post('/login ', function(req,res){
                 var salt=dbString.split('$')[2];
                 var hashedPassword =hash(password, salt);
                 if(hashedPassword ===dbString){
+                    //Set the session
+                    req.session.auth ={userId:result.rows[0].id};
+                    //set cookie
+                    
                     res.send('credentials correct');
                 }else{
                     res.send(403).send('username/password is invalid');
@@ -142,6 +151,13 @@ app.post('/login ', function(req,res){
             }
         }
     });
+});
+app.get('/check-login', function(req,res){
+    if(req.session && req.session.auth.userId){
+        res.send('You are logged in:' + req.session.auth.userId.toString());
+    }else{
+        res.send('You are not logged in');
+    }
 });
 var pool = new Pool(config);
 app.get('/test-db',function(req,res)  {
